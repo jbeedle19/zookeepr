d3c11ed6aef795d1f146a9fe8635f54a61796d13
jbeedle19 <josh.beedle@gmail.com>
2021-01-12 10:43:31 -0500

    added validation

--- server.js
+++ server.js
@@ -65,6 +65,22 @@ function createNewAnimal(body, animalsArray) {
     return animal;
 }
 
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -87,10 +103,14 @@ app.post('/api/animals', (req, res) => {
     // Set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    // Add animal to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
-
-    res.json(animal);
+    // If any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // Add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
 });
 
 app.listen(PORT, () => {
