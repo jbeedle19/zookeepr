aff1f558a9b3a51dd20629d199850c346bf077a3
jbeedle19 <josh.beedle@gmail.com>
2021-01-12 10:33:23 -0500

    added data using post

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// Parse incoming JSON data
+app.use(express.json());
 
 const { animals } = require('./data/animals');
 
@@ -48,6 +55,16 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -67,8 +84,13 @@ app.get('/api/animals/:id', (req, res) => {
 
 app.post('/api/animals', (req, res) => {
     // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
+    // Set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // Add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
 });
 
 app.listen(PORT, () => {
